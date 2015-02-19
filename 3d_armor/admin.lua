c03cefbc87072c1769e11246b35e6f6e8c2de92f
stujones11 <stujones111@gmail.com>
2015-02-19 22:11:03 +0000

    Don't show admin armor in the creative inventory

--- 3d_armor/admin.lua
+++ 3d_armor/admin.lua
@@ -6,28 +6,28 @@ minetest.register_alias("adminlegginss","3d_armor:leggings_admin")
 minetest.register_tool("3d_armor:helmet_admin", {
 	description = "Admin Helmet",
 	inventory_image = "3d_armor_inv_helmet_admin.png",
-	groups = {armor_head=1000, armor_heal=1000, armor_use=0, not_in_creative=0},
+	groups = {armor_head=1000, armor_heal=1000, armor_use=0, not_in_creative_inventory=1},
 	wear = 0,
 })
 
 minetest.register_tool("3d_armor:chestplate_admin", {
 	description = "Admin Chestplate",
 	inventory_image = "3d_armor_inv_chestplate_admin.png",
-	groups = {armor_torso=1000, armor_heal=1000, armor_use=0, not_in_creative=0},
+	groups = {armor_torso=1000, armor_heal=1000, armor_use=0, not_in_creative_inventory=1},
 	wear = 0,
 })
 
 minetest.register_tool("3d_armor:leggings_admin", {
 	description = "Admin Leggings",
 	inventory_image = "3d_armor_inv_leggings_admin.png",
-	groups = {armor_legs=1000, armor_heal=1000, armor_use=0, not_in_creative=0},
+	groups = {armor_legs=1000, armor_heal=1000, armor_use=0, not_in_creative_inventory=1},
 	wear = 0,
 })
 
 minetest.register_tool("3d_armor:boots_admin", {
 	description = "Admin Boots",
 	inventory_image = "3d_armor_inv_boots_admin.png",
-	groups = {armor_feet=1000, armor_heal=1000, armor_use=0, not_in_creative=0},
+	groups = {armor_feet=1000, armor_heal=1000, armor_use=0, not_in_creative_inventory=1},
 	wear = 0,
 })
 
