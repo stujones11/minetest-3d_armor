991d2955e034e5b3e0b498d5195eb9923b770937
stujones11 <stujones111@gmail.com>
2015-12-04 20:36:42 +0000

    Simplify previous commit

--- 3d_armor/armor.lua
+++ 3d_armor/armor.lua
@@ -171,10 +171,7 @@ armor.set_player_armor = function(self, player)
 				if v == false then
 					local level = def.groups["armor_"..k]
 					if level then
-						local texture = def.texture
-						if not texture then
-							texture = item:gsub("%:", "_")
-						end
+						local texture = def.texture or item:gsub("%:", "_")
 						table.insert(textures, texture..".png")
 						preview = preview.."^"..texture.."_preview.png"
 						armor_level = armor_level + level
