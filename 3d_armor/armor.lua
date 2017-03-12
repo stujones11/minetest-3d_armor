7a980a671767b10200b0c9a83dc95acf60751db1
stujones11 <stujones111@gmail.com>
2017-03-12 20:34:01 +0000

    Fix missing sfinv page background images, closes #90

--- 3d_armor/armor.lua
+++ 3d_armor/armor.lua
@@ -79,6 +79,10 @@ armor = {
 }
 
 local armor_formpage = "image[2.5,0;2,4;armor_preview]"..
+	default.gui_bg..
+	default.gui_bg_img..
+	default.gui_slots..
+	default.get_hotbar_bg(0,4.25)..
 	"label[5,1;Level: armor_level]"..
 	"label[5,1.5;Heal:  armor_heal]"..
 	"label[5,2;Fire:  armor_fire]"..
@@ -87,12 +91,7 @@ local armor_formpage = "image[2.5,0;2,4;armor_preview]"..
 	"list[current_player;main;0,5.5;8,3;8]"
 if minetest.get_modpath("inventory_plus") then
 	inv_mod = "inventory_plus"
-	armor.formspec = "size[8,8.5]button[6,0;2,0.5;main;Back]"..
-		default.gui_bg..
-		default.gui_bg_img..
-		default.gui_slots..
-		default.get_hotbar_bg(0,4.25)..
-		armor_formpage
+	armor.formspec = "size[8,8.5]button[6,0;2,0.5;main;Back]"..armor_formpage
 	if minetest.get_modpath("crafting") then
 		inventory_plus.get_formspec = function(player, page)
 		end
