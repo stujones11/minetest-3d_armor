53dd6e7b41647131de407c0cc2150ecca7690061
y <y@y>
2019-06-09 04:55:21 +0100

    make fire protect a default option

--- 3d_armor/api.lua
+++ 3d_armor/api.lua
@@ -95,7 +95,7 @@ armor.config = {
 	material_mithril = true,
 	material_crystal = true,
 	water_protect = true,
-	fire_protect = minetest.get_modpath("ethereal") ~= nil,
+	fire_protect = true,
 	punch_damage = true,
 }
 
